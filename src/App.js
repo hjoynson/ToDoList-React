b5a3fa2d2066c3d3de79f69a77410781011a5cfb
Hannah Joynson <hannahjoynson@gmail.com>
2022-05-12 12:20:16 +0100

    functions and objects added

--- src/App.js
+++ src/App.js
@@ -8,12 +8,41 @@ function App() {
   const [todos, setTodos] = React.useState([])
   const [todo, setTodo] = React.useState("")
 
+  function handleSubmit(e) {
+   e.preventDefault() 
+
+  //  object created
+   const newTodo = {
+     id: new Date().getTime(),
+     text:todo,
+     completed:false,
+   }
+
+// updates todo list
+
+setTodos([...todos].concat(newTodo))
+setTodo("")
+
+  }
+
   return (
     <div className="App">
-      <form>
-          <input type=" text" />
+
+      <form onSubmit={handleSubmit}>
+
+        {/* Any text written applied to the hook variable */}
+        {/* The function will run everytime something is typed in */}
+        {/* Value of input is whatever the todo value is */}
+
+          <input type=" text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
             <button type="submit">add Todo</button>
       </form>
+
+
+      {/* Display todos to the screen */}
+
+    
+      {todos.map((todo) => <div>{todo.text}</div>)}
     </div>
   );
 }
